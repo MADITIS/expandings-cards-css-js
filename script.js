c02c3b466551a88d104c668f50dbc4b46e7738b9
MDS <manik6418@gmail.com>
2023-02-03 13:40:23 +0530

    final

---
+++ script.js
@@ -0,0 +1,16 @@
+const card = document.querySelectorAll(".card")
+const cards = document.querySelector('.cards')
+
+cards.addEventListener("mousedown", mousedownHanlder)
+
+function mousedownHanlder(event) {
+    let target = event.target
+    if (target.parentElement == cards) {
+        target.classList.add("active")
+        Array.from(card).forEach((value) => {
+            if (value != target) {
+                value.classList.remove("active")
+            }
+        })
+    }
+}
